b167346bc9d1753e174572bbb47413cbc7b7d63a
Megan Yap <megany128@gmail.com>
2022-10-24 23:03:44 +0800

    name change

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 export default {
   expo: {
     name: 'SG Booths',
-    slug: 'SG Booths',
+    slug: 'SGBooths',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
